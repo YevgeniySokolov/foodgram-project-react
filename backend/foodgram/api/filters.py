9bbc8fa8fdca3227b7ae36cc875245654918241d
Yevgeniy Sokolov <ea.sokolov.87@yandex.ru>
2024-05-20 04:48:51 +0300

    Version 18 (4 failed)

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -13,7 +13,19 @@ class RecipeFilter(filters.FilterSet):
         to_field_name='slug',
         queryset=Tag.objects.all()
     )
+    is_favorited = filters.BooleanFilter(method='favorited')
+    is_in_shopping_cart = filters.BooleanFilter(method='in_cart')
 
     class Meta:
         model = Recipe
         exclude = ('image',)
+
+    def favorited(self, queryset, name, value):
+        if value and self.request.user.is_authenticated:
+            return queryset.filter(favoriterecipes__user=self.request.user)
+        return queryset
+
+    def in_cart(self, queryset, name, value):
+        if value and self.request.user.is_authenticated:
+            return queryset.filter(shoppingcarts__user=self.request.user)
+        return queryset
